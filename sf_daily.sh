13702d918a76f6a191f8d211902562a58e55f7d1
eric-sf <eric.chen@sfgov.org>
2016-10-20 14:29:33 -0700

    Delete

--- sf_daily.sh
+++
@@ -1,9 +0,0 @@
-cd /home/webadmin/analytics/analytics-reporter
-
-export ANALYTICS_REPORT_EMAIL="analytics@ordinal-shield-130316.iam.gserviceaccount.com"
-export ANALYTICS_REPORT_IDS="ga:17386173"
-export ANALYTICS_KEY_PATH="Pantheon-34f2eb823685.json"
-
-/usr/bin/nodejs /home/webadmin/analytics/analytics-reporter/bin/analytics --output /var/www/html/analytics/data/ --frequency=daily --slim --verbose
-
-/usr/bin/nodejs /home/webadmin/analytics/analytics-reporter/bin/analytics --output /var/www/html/analytics/data/ --frequency=daily --slim --verbose --csv
